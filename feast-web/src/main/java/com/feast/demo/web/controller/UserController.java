6e95113659d6fd543b8b2a70f967f594ad2eaa3c
aries <dtmt@vip.sina.com>
2017-05-14 21:32:30 +0800

    matao

---
+++ feast-web/src/main/java/com/feast/demo/web/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.feast.demo.web.controller;
+
+import com.alibaba.dubbo.common.utils.StringUtils;
+import com.alibaba.fastjson.JSON;
+import com.feast.demo.ad.entity.TAd;
+import com.feast.demo.web.entity.UserObj;
+import com.feast.demo.web.memory.LoginMemory;
+import com.feast.demo.web.service.UserService;
+import com.google.common.collect.Maps;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * Created by ggke on 2017/4/10.
+ */
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public String regUser(@ModelAttribute("user") UserObj user) {
+        System.out.println("androidID is:"+user.getAndroidID());
+        System.out.println("imei is:"+user.getImei());
+        System.out.println("ipv4 is:"+user.getIpv4());
+        System.out.println("mac is:"+user.getMac());
+        System.out.println("mobileNO is:"+user.getMobileNO());
+
+        UserObj resultObj = userService.getStatus(user,"register");
+        return JSON.toJSONString(resultObj);
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String loginUser(@ModelAttribute("user") UserObj user) {
+        System.out.println("androidID is:"+user.getAndroidID());
+        System.out.println("imei is:"+user.getImei());
+        System.out.println("ipv4 is:"+user.getIpv4());
+        System.out.println("mac is:"+user.getMac());
+        System.out.println("mobileNO is:"+user.getMobileNO());
+
+        UserObj resultObj = userService.getStatus(user,"login");
+
+        if("0".equals(resultObj.getResultCode())){
+            LoginMemory.set(user.getMobileNO(),user);
+        }
+        return JSON.toJSONString(resultObj);
+    }
+
+
+}
---
+++ feast-web/src/main/java/com/feast/demo/web/service/UserService.java
@@ -0,0 +1,35 @@
+package com.feast.demo.web.service;
+
+import org.springframework.stereotype.Service;
+import com.feast.demo.web.entity.UserObj;
+/**
+ * Created by pinyou on 17-4-11.
+ */
+
+@Service
+public class UserService {
+
+
+    public UserObj getStatus(UserObj user,String flag){
+
+        System.out.println("androidID is:"+user.getAndroidID());
+        System.out.println("imei is:"+user.getImei());
+        System.out.println("ipv4 is:"+user.getIpv4());
+        System.out.println("mac is:"+user.getMac());
+        System.out.println("mobileNO is:"+user.getMobileNO());
+        if("register".equals(flag)){
+            System.out.println("register");
+        }else{
+            System.out.println("login");
+        }
+
+
+
+        user.setResultCode("0");
+        user.setToken("ljiqsdgf54sdfweq6565f7wes51635sad4f65f");
+        return user;
+    }
+
+
+
+}
