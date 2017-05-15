4b6b4297a45417a88a08cab1881c90ed5a2ab3e8
ggke <123033934@qq.com>
2017-05-16 00:07:04 +0800

    代码保存

---
+++ feast-web/src/main/java/com/feast/demo/web/service/PersonalStatisticsService.java
@@ -0,0 +1,18 @@
+package com.feast.demo.web.service;
+
+import com.feast.demo.web.entity.CoordinatePoint;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by ggke on 2017/5/15.
+ */
+@Service
+public class PersonalStatisticsService {
+
+    public List<CoordinatePoint> getCharts(){
+
+        return null;
+    }
+}
