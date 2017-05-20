cabcb6662ae81538c25b3e4cd69fa13060f07195
aries <dtmt@vip.sina.com>
2017-05-20 18:06:09 +0800

    matao

--- feast-web/src/main/java/com/feast/demo/web/controller/UserController.java
+++ feast-web/src/main/java/com/feast/demo/web/controller/UserController.java
@@ -1,17 +1,15 @@
 package com.feast.demo.web.controller;
 
-import com.alibaba.dubbo.common.utils.StringUtils;
 import com.alibaba.fastjson.JSON;
-import com.feast.demo.ad.entity.TAd;
 import com.feast.demo.web.entity.UserObj;
 import com.feast.demo.web.memory.LoginMemory;
 import com.feast.demo.web.service.UserService;
-import com.google.common.collect.Maps;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
-import java.util.Map;
 
 /**
  * Created by ggke on 2017/4/10.
@@ -52,5 +50,20 @@ public class UserController {
         return JSON.toJSONString(resultObj);
     }
 
+    @RequestMapping(value = "/logout",method = RequestMethod.POST)
+    public String logoutUser(@ModelAttribute("user") UserObj user) {
+        System.out.println("androidID is:"+user.getAndroidID());
+        System.out.println("imei is:"+user.getImei());
+        System.out.println("ipv4 is:"+user.getIpv4());
+        System.out.println("mac is:"+user.getMac());
+        System.out.println("token is:"+user.getToken());
+
+        UserObj resultObj = userService.getStatus(user,"logout");
+
+        if("0".equals(resultObj.getResultCode())){
+            LoginMemory.set(user.getMobileNO(),user);
+        }
+        return JSON.toJSONString(resultObj);
+    }
 
 }
--- feast-web/src/main/java/com/feast/demo/web/service/UserService.java
+++ feast-web/src/main/java/com/feast/demo/web/service/UserService.java
@@ -9,27 +9,54 @@ import com.feast.demo.web.entity.UserObj;
 @Service
 public class UserService {
 
+    public UserObj getStatus(UserObj user, String flag) {
 
-    public UserObj getStatus(UserObj user,String flag){
-
-        System.out.println("androidID is:"+user.getAndroidID());
-        System.out.println("imei is:"+user.getImei());
-        System.out.println("ipv4 is:"+user.getIpv4());
-        System.out.println("mac is:"+user.getMac());
-        System.out.println("mobileNO is:"+user.getMobileNO());
-        if("register".equals(flag)){
-            System.out.println("register");
-        }else{
-            System.out.println("login");
+        System.out.println("androidID is:" + user.getAndroidID());
+        System.out.println("imei is:" + user.getImei());
+        System.out.println("ipv4 is:" + user.getIpv4());
+        System.out.println("mac is:" + user.getMac());
+        System.out.println("mobileNO is:" + user.getMobileNO());
+        if ("register".equals(flag) || "login".equals(flag)) {
+            String mobileNo = user.getMobileNO();
+            if (!"".equals(mobileNo)) {
+                if ("register".equals(flag)) {
+                    if ("13301018888".equals(mobileNo)) {
+                        user.setResultCode("0");
+                        user.setToken("ljiqsdgf54sdfweq6565f7wes51635sad4f65f");
+                    } else {
+                        user.setResultCode("1");
+                        user.setResultMsg("该手机号已注册，请更换手机号！");
+                    }
+                } else if ("login".equals(flag)) {
+                    if ("13301019999".equals(mobileNo)) {
+                        user.setResultCode("0");
+                        user.setToken("ljiqsdgf54sdfweq6565f7wes51635sad4f65f");
+                    } else {
+                        user.setResultCode("1");
+                        user.setResultMsg("该手机号已注册，请更换手机号！");
+                    }
+                } else {
+                    user.setResultCode("1");
+                    user.setResultMsg("非法操作，请重试！");
+                }
+            } else {
+                user.setResultCode("1");
+                user.setResultMsg("手机号为空！");
+            }
+        }
+        if("logout".equals(flag)){
+            String token = user.getToken();
+            if("ljiqsdgf54sdfweq6565f7wes51635sad4f65f".equals(token)){
+                user.setResultCode("0");
+                user.setResultMsg("成功退出");
+            }else{
+                user.setResultCode("1");
+                user.setResultMsg("退出异常，请重新尝试！");
+            }
         }
 
-
-
-        user.setResultCode("0");
-        user.setToken("ljiqsdgf54sdfweq6565f7wes51635sad4f65f");
         return user;
+
     }
-
-
-
 }
+
