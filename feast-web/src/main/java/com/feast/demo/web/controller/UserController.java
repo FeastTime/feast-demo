60012bcdc5bb352a81e4ea536b2e285e0d30d3e3
ggke <123033934@qq.com>
2017-06-04 20:33:51 +0800

    参数调整

--- feast-web/src/main/java/com/feast/demo/web/controller/UserController.java
+++ feast-web/src/main/java/com/feast/demo/web/controller/UserController.java
@@ -6,6 +6,7 @@ import com.feast.demo.web.entity.UserObj;
 import com.feast.demo.web.memory.LoginMemory;
 import com.feast.demo.web.service.UserService;
 import com.feast.demo.web.util.StringUtils;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,16 +41,16 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping(value = "/login",method = RequestMethod.POST,produces="text/html;charset=UTF-8")
-    public String loginUser(@RequestBody String text){
-        text = StringUtils.decode(text);
-        JSONObject jsono  = JSON.parseObject(text);
-        System.out.println("androidID is:"+jsono.getString("androidID"));
-        System.out.println("imei is:"+jsono.getString("imei"));
-        System.out.println("ipv4 is:"+jsono.getString("ipv4"));
-        System.out.println("mac is:"+jsono.getString("mac"));
-        System.out.println("mobileNO is:"+jsono.getString("mobileNO"));
+    public String loginUser(@ModelAttribute("user")UserObj user){
+//        text = StringUtils.decode(text);
+//        JSONObject jsono  = JSON.parseObject(text);
+        System.out.println("androidID is:"+user.getAndroidID());
+        System.out.println("imei is:"+user.getImei());
+        System.out.println("ipv4 is:"+user.getIpv4());
+        System.out.println("mac is:"+user.getMac());
+        System.out.println("mobileNO is:"+user.getMobileNO());
 
-        UserObj resultObj = userService.getStatus(jsono,"login");
+        UserObj resultObj = userService.getStatus(JSON.parseObject(JSON.toJSONString(user)),"login");
 
         if("0".equals(resultObj.getResultCode())){
             LoginMemory.set(resultObj.getMobileNO(),resultObj);
@@ -77,4 +78,4 @@ public class UserController {
         }
         return JSON.toJSONString(resultObj);
     }
-}
+    }
