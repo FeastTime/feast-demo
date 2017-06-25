1e6d3523846a6ff1ff27b956cc6c4112e18caef7
ggke <123033934@qq.com>
2017-06-25 12:06:02 +0800

    ad项目添加数据库操作相关代码（spring-data-jpa）;dubbo接口发布改为注解形式；

---
+++ feast-ad/src/main/java/com/feast/demo/ad/dao/AdvertisementDaoCustom.java
@@ -0,0 +1,13 @@
+package com.feast.demo.ad.dao;
+
+import com.feast.demo.ad.entity.Advertisement;
+
+import java.util.List;
+
+/**
+ * Created by ggke on 2017/6/25.
+ */
+public interface AdvertisementDaoCustom {
+
+    public List<Advertisement> findAll2();
+}
