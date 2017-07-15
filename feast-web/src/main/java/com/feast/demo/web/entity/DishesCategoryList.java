eea3447e11e05777b859434375d69282e68ae8c9
aries <dtmt@vip.sina.com>
2017-07-15 20:42:44 +0800

    matao

---
+++ feast-web/src/main/java/com/feast/demo/web/entity/DishesCategoryList.java
@@ -0,0 +1,26 @@
+package com.feast.demo.web.entity;
+
+/**
+ * Created by aries on 2017/5/14.
+ */
+public class DishesCategoryList {
+
+    private String categoryName;
+    private String classType;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+}
---
+++ feast-web/src/main/java/com/feast/demo/web/entity/DishesCategoryObj.java
@@ -0,0 +1,73 @@
+package com.feast.demo.web.entity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aries on 2017/5/14.
+ */
+public class DishesCategoryObj {
+
+    private String imei;
+    private String androidID;
+    private String ipv4;
+    private String mac;
+    private String resultCode;
+    private String resultMsg;
+    private ArrayList dishesCategoryList;
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public String getAndroidID() {
+        return androidID;
+    }
+
+    public void setAndroidID(String androidID) {
+        this.androidID = androidID;
+    }
+
+    public String getIpv4() {
+        return ipv4;
+    }
+
+    public void setIpv4(String ipv4) {
+        this.ipv4 = ipv4;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+
+    public void setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+    }
+
+    public ArrayList getDishesCategoryList() {
+        return dishesCategoryList;
+    }
+
+    public void setDishesCategoryList(ArrayList dishesCategoryList) {
+        this.dishesCategoryList = dishesCategoryList;
+    }
+}
