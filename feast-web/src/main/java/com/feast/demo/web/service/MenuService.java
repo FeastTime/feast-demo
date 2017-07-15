a5de393d002714c55ed91a7d8b7bc76d179d3df4
aries <dtmt@vip.sina.com>
2017-07-15 20:06:09 +0800

    matao

--- feast-web/src/main/java/com/feast/demo/web/service/MenuService.java
+++ feast-web/src/main/java/com/feast/demo/web/service/MenuService.java
@@ -32,7 +32,7 @@ public class MenuService {
         String mobileNO = jsonObj.getString("mobileNO");
 
         MenuObj menuObj = new MenuObj();
-        if ("13388996666".equals(mobileNO)) {
+//        if ("13388996666".equals(mobileNO)) {
             /**
              * 1001 鸡肉
              * 1002 牛肉
@@ -50,15 +50,15 @@ public class MenuService {
 
                 DishesBean.setDishID("00000001");
                 DishesBean.setDishNO("000000001");
-                DishesBean.setDishImgUrl("http://aa.jpg");
-                DishesBean.setTvUrl("http://aaa.mp4");
+                DishesBean.setDishImgUrl("glcwdcjl.jpg");
+                DishesBean.setTvUrl("glcwdcjl.mp4");
                 DishesBean.setHotFlag("0");
                 DishesBean.setMaterialFlag("0");
                 DishesBean.setTitleADImgUrl("http://aaa.jpg");
                 DishesBean.setTitleADUrl("http://aaa.html");
                 DishesBean.setEatTimes("4");
-                DishesBean.setDishName(StringUtils.encode("回锅肉"));
-                DishesBean.setDetail(StringUtils.encode("回锅肉是一种烹调猪肉的四川传统菜式，属于川菜系。起源四川农村地区[1]  。古代时期称作油爆锅；四川地区大部分家庭都能制作。回锅肉的特点是：口味独特，色泽红亮，肥而不腻，入口浓香。所谓回锅，就是再次烹调的意思。回锅肉在川菜中的地位是非常重要的。回锅肉一直被认为是川菜之首，川菜之化身，提到川菜必然想到回锅肉。它色香味俱全，颜色养眼，是下饭菜之首选。配料各有不同，除了蒜苗（青蒜）还可以用彩椒，洋葱，韭菜，锅盔等来制作回锅肉，每家都有自己的秘方"));
+                DishesBean.setDishName(StringUtils.encode("橄榄菜豌豆炒鸡柳"));
+                DishesBean.setDetail(StringUtils.encode("其实将橄榄菜炒四季豆换为豌豆亦可，豌豆更香甜，加入鸡柳就成为一道很好下饭的家常小菜。"));
                 DishesBean.setTime("11:00-13:00");
                 DishesBean.setCost("45.00");
                 DishesBean.setPrice("33.00");
@@ -72,15 +72,19 @@ public class MenuService {
 
                 menuObj.setDishesList(dishesList);
 
-            } else if ("002".equals(classType)) {
+            } else if ("1002".equals(classType)) {
+                menuObj.setResultCode("0");
+            }else if ("1003".equals(classType)) {
+                menuObj.setResultCode("0");
+            }else if ("1004".equals(classType)) {
                 menuObj.setResultCode("0");
             }
 
-        } else if ("13388998888".equals(mobileNO)) {
-            {
-                menuObj.setResultCode("0");
-            }
-        }
+//        } else if ("13388998888".equals(mobileNO)) {
+//            {
+//                menuObj.setResultCode("0");
+//            }
+//        }
 
         return menuObj;
     }
