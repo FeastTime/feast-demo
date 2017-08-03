27e924290a4509dc7d93e8e0022f07594edbb3d6
ke.gu <pinyou@pinyou-System-Product-Name>
2017-08-03 15:20:42 +0800

    登陆代码调整

--- feast-web/src/main/java/com/feast/demo/web/controller/UserController.java
+++ feast-web/src/main/java/com/feast/demo/web/controller/UserController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.Map;
 
 /**
@@ -64,38 +62,40 @@ public class UserController {
         System.out.println("转之前"+text);
         text = StringUtils.decode(text);
         System.out.println("转之后"+text);
-        JSONObject jsono  = JSON.parseObject(text);
-        System.out.println("androidID is:"+jsono.getString("androidID"));
-        System.out.println("imei is:"+jsono.getString("imei"));
-        System.out.println("ipv4 is:"+jsono.getString("ipv4"));
-        System.out.println("mac is:"+jsono.getString("mac"));
-        System.out.println("mobileNO is:"+jsono.getString("mobileNO"));
-        System.out.println("imei is:"+jsono.getString("imei"));
-        Long mobileNo = jsono.getLong("mobileNO");
-      //  UserObj resultObj = userService.getStatus(jsono,"login");
+        User user = JSONObject.parseObject(text,User.class);
+//        JSONObject jsono  = JSON.parseObject(text);
+//        System.out.println("androidID is:"+jsono.getString("androidID"));
+//        System.out.println("imei is:"+jsono.getString("imei"));
+//        System.out.println("ipv4 is:"+jsono.getString("ipv4"));
+//        System.out.println("mac is:"+jsono.getString("mac"));
+//        System.out.println("mobileNO is:"+jsono.getString("mobileNO"));
+//        System.out.println("imei is:"+jsono.getString("imei"));
+//        Long mobileNo = jsono.getLong("mobileNO");
+
         String resultMsg = "";
-        String imei = jsono.getString("imei");
         Boolean success = true;
-        if(imei != null){
-            Device device = deviceService.findDeviceInfoByImei(imei);
+        Device device = deviceService.findDeviceInfoByImei(user.getImei());
+        if(device != null && device.getStore() != null){
             result.put("storeId",device.getStore().getId());
+        }else{
+            result.put("storeId",null);
         }
         //访客
-        if(mobileNo == null){
+        if(user.getMobileNo() == null){
             resultMsg = "访客登录成功";
             result.put("token","fangketoken:asieurqknro239480984234lkasj");
             success = true;
-        }//用户登录
-        else{
-            User user = userService.fingByMobileNo(mobileNo);
-            if(user == null){
-                resultMsg = "您的手机号没有注册，请注册";
-                success = false;
-            }else{
-                LoginMemory.set(user.getMobileNo()+"",user);
-                resultMsg = "欢迎您登录成功!";
-                result.put("token","token:asieurqknro239480984234lkasj");
+        }else{
+            //用户登录
+            User _user = userService.fingByMobileNo(user.getMobileNo());
+            //如果没有注册，则自动注册用户
+            if(_user == null){
+                userService.createUser(user);
             }
+            LoginMemory.set(user.getMobileNo()+"",user);
+            resultMsg = "欢迎您登录成功!";
+            result.put("token","token:asieurqknro239480984234lkasj");
+
         }
         result.put("resultCode",success);
         result.put("resultMsg",resultMsg);
