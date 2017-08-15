d0e156872e7d6b4979e51db994475875552f2262
aries <dtmt@vip.sina.com>
2017-08-16 00:07:33 +0800

    matao

---
+++ feast-menu/src/main/java/com/feast/demo/menu/entity/MenuAuxiliary.java
@@ -0,0 +1,26 @@
+package com.feast.demo.menu.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by aries on 2017/8/15.
+ */@Entity
+@Data
+@Table(name = "menuauxiliary")
+public class MenuAuxiliary implements Serializable {
+
+    private String dishid;
+    private String hotflag;
+    private String eattimes;
+    private String discountstime;
+    private BigDecimal price;
+    private String sales;
+    private String starlevel;
+    private String tmpid;
+    private String pageid;
+}
