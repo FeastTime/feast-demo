e00838441ce7bed4f6bc0708c460e4e555966cf3
aries <dtmt@vip.sina.com>
2017-08-16 00:09:25 +0800

    matao

--- feast-menu/src/main/java/com/feast/demo/menu/dao/MenuDao.java
+++ feast-menu/src/main/java/com/feast/demo/menu/dao/MenuDao.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * Created by aries on 2017/8/12.
  */
-public interface MenuDao extends PagingAndSortingRepository<Menu,Long>,MenuDaoCustom {
+public interface MenuDao extends PagingAndSortingRepository<Menu,String>,MenuDaoCustom {
 
     public String getMenuCountByCategoryIdAndStoreId(String categoryId, String storeId);
 
