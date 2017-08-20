70910d807e47e66e457337e2d33c9033e1705de0
ggke <123033934@qq.com>
2017-08-20 11:06:29 +0800

    回撤无效代码

--- feast-web/src/main/java/com/feast/demo/web/service/DishesCategoryService.java
+++ feast-web/src/main/java/com/feast/demo/web/service/DishesCategoryService.java
@@ -1,46 +1,52 @@
 package com.feast.demo.web.service;
 
 import com.alibaba.fastjson.JSONObject;
+import com.feast.demo.menu.vo.DishesCategoryVo;
+import com.feast.demo.web.entity.DishesCategoryBean;
 import com.feast.demo.web.entity.DishesCategoryObj;
+import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by aries on 17-8-6.
+ * Created by matao on 2017/8/6.
  */
 
 @Service
 public class DishesCategoryService {
 
     @Autowired
-    private com.feast.demo.menu.service.MenuService menuRemoteService;
+    private com.feast.demo.menu.service.DishesCategoryService dishesCategoryRemoteService;
 
-    public DishesCategoryObj findDishesCategoryByStoreid(JSONObject jsonObj) {
-        String  StoreId= jsonObj.getString("storeId");
+    public DishesCategoryObj findDishesCategoryByStoreId(JSONObject jsonObj) {
+        System.out.println("storeId is:" + jsonObj.getString("storeId"));
+
         DishesCategoryObj dishesCategoryObj = new DishesCategoryObj();
-//        ArrayList<DishesCategory> dishesCategoryList = menuRemoteService.findDishesCategoryByStoreid(StoreId);
-//        List<DishesCategoryList> dishesCategoryBeanList = Lists.newArrayList();
-//        try {
-//            if (dishesCategoryList != null && dishesCategoryList.size() > 0) {
-//                dishesCategoryObj.setResultCode("0");
-//                for (int i = 0; i < dishesCategoryList.size(); i++) {
-//                    DishesCategory dc = dishesCategoryList.get(i);
-//                    DishesCategoryList dishesCategoryListBean = new DishesCategoryList();
-//                    dishesCategoryListBean.setCategoryName(dc.getCategoryname());
-//                    dishesCategoryListBean.setCategoryId(dc.getCategoryid()+"");
-//                    dishesCategoryBeanList.add(dishesCategoryListBean);
-//
-//                }
-//                dishesCategoryObj.setDishesCategoryList((ArrayList) dishesCategoryBeanList);
-//            } else {
-//                dishesCategoryObj.setResultCode("0");
-//                dishesCategoryObj.setDishesCategoryList(null);
-//            }
-//        }catch (Exception e){
-//            dishesCategoryObj.setResultCode("1");
-//            e.printStackTrace();
-//        }
+        try {
+            List<DishesCategoryVo> list = dishesCategoryRemoteService.findDishesCategoryByStoreId(jsonObj);
+            if (list != null && list.size() > 0) {
+                dishesCategoryObj.setResultCode("0");
+                DishesCategoryBean dishesCategoryBean = null;
+                ArrayList<DishesCategoryBean> dishesCategoryBeanList = Lists.newArrayList();
+                for (int i = 0; i < list.size(); i++) {
+                    DishesCategoryVo dishesCategoryVo = (DishesCategoryVo)list.get(i);
+                    dishesCategoryBean = new DishesCategoryBean();
+                    dishesCategoryBean.setCategoryId(dishesCategoryVo.getCategoryId());
+                    dishesCategoryBean.setCategoryName(dishesCategoryVo.getCategoryName());
+                    dishesCategoryBeanList.add(dishesCategoryBean);
+                }
+                dishesCategoryObj.setDishesCategoryList(dishesCategoryBeanList);
+            } else {
+                dishesCategoryObj.setResultCode("0");
+                dishesCategoryObj.setDishesCategoryList(null);
+            }
+        }catch (Exception e){
+            dishesCategoryObj.setResultCode("1");
+            e.printStackTrace();
+        }
         return dishesCategoryObj;
     }
-
 }
