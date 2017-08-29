2946243f3012707fdb07ef27659d51c58c2452f3
hoopster <hoopster@163.com>
2017-08-29 22:51:16 +0800

    order

--- feast-web/src/main/java/com/feast/demo/web/entity/RecommendDishObj.java
+++ feast-web/src/main/java/com/feast/demo/web/entity/RecommendDishObj.java
@@ -4,38 +4,43 @@ package com.feast.demo.web.entity;
  * Created by Administrator on 2017/5/23.
  */
 public class RecommendDishObj {
-    
-    private String dishID;
-    private String dishNO;
-    private String dishName;
+
+    private String dishId;
+    private String dishNo;
     private String dishImgUrl;
-    private String todayPrice;
-    private String amount;
-    private String beforeOrderTimes;
-    private String extraFlag;
+    private String tvUrl;
+    private String hotFlag;
+    private String materialFlag;
+    private String titleAdImgUrl;
+    private String titleAdUrl;
+    private String eatTimes;
+    private String dishName;
+    private String detail;
+    private String discountsTime;
+    private String cost;
+    private String price;
+    private String sales;
+    private String waitTime;
+    private String exponent;
+    private String starLevel;
+    private String pungencyDegree;
+    private String tmpId;
+    private String pageId;
 
-    public String getDishID() {
-        return dishID;
+    public String getDishId() {
+        return dishId;
     }
 
-    public void setDishID(String dishID) {
-        this.dishID = dishID;
+    public void setDishId(String dishId) {
+        this.dishId = dishId;
     }
 
-    public String getDishNO() {
-        return dishNO;
+    public String getDishNo() {
+        return dishNo;
     }
 
-    public void setDishNO(String dishNO) {
-        this.dishNO = dishNO;
-    }
-
-    public String getDishName() {
-        return dishName;
-    }
-
-    public void setDishName(String dishName) {
-        this.dishName = dishName;
+    public void setDishNo(String dishNo) {
+        this.dishNo = dishNo;
     }
 
     public String getDishImgUrl() {
@@ -46,35 +51,147 @@ public class RecommendDishObj {
         this.dishImgUrl = dishImgUrl;
     }
 
-    public String getTodayPrice() {
-        return todayPrice;
+    public String getTvUrl() {
+        return tvUrl;
     }
 
-    public void setTodayPrice(String todayPrice) {
-        this.todayPrice = todayPrice;
+    public void setTvUrl(String tvUrl) {
+        this.tvUrl = tvUrl;
     }
 
-    public String getAmount() {
-        return amount;
+    public String getHotFlag() {
+        return hotFlag;
     }
 
-    public void setAmount(String amount) {
-        this.amount = amount;
+    public void setHotFlag(String hotFlag) {
+        this.hotFlag = hotFlag;
     }
 
-    public String getBeforeOrderTimes() {
-        return beforeOrderTimes;
+    public String getMaterialFlag() {
+        return materialFlag;
     }
 
-    public void setBeforeOrderTimes(String beforeOrderTimes) {
-        this.beforeOrderTimes = beforeOrderTimes;
+    public void setMaterialFlag(String materialFlag) {
+        this.materialFlag = materialFlag;
     }
 
-    public String getExtraFlag() {
-        return extraFlag;
+    public String getTitleAdImgUrl() {
+        return titleAdImgUrl;
     }
 
-    public void setExtraFlag(String extraFlag) {
-        this.extraFlag = extraFlag;
+    public void setTitleAdImgUrl(String titleAdImgUrl) {
+        this.titleAdImgUrl = titleAdImgUrl;
+    }
+
+    public String getTitleAdUrl() {
+        return titleAdUrl;
+    }
+
+    public void setTitleAdUrl(String titleAdUrl) {
+        this.titleAdUrl = titleAdUrl;
+    }
+
+    public String getEatTimes() {
+        return eatTimes;
+    }
+
+    public void setEatTimes(String eatTimes) {
+        this.eatTimes = eatTimes;
+    }
+
+    public String getDishName() {
+        return dishName;
+    }
+
+    public void setDishName(String dishName) {
+        this.dishName = dishName;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getDiscountsTime() {
+        return discountsTime;
+    }
+
+    public void setDiscountsTime(String discountsTime) {
+        this.discountsTime = discountsTime;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSales() {
+        return sales;
+    }
+
+    public void setSales(String sales) {
+        this.sales = sales;
+    }
+
+    public String getWaitTime() {
+        return waitTime;
+    }
+
+    public void setWaitTime(String waitTime) {
+        this.waitTime = waitTime;
+    }
+
+    public String getExponent() {
+        return exponent;
+    }
+
+    public void setExponent(String exponent) {
+        this.exponent = exponent;
+    }
+
+    public String getStarLevel() {
+        return starLevel;
+    }
+
+    public void setStarLevel(String starLevel) {
+        this.starLevel = starLevel;
+    }
+
+    public String getPungencyDegree() {
+        return pungencyDegree;
+    }
+
+    public void setPungencyDegree(String pungencyDegree) {
+        this.pungencyDegree = pungencyDegree;
+    }
+
+    public String getTmpId() {
+        return tmpId;
+    }
+
+    public void setTmpId(String tmpId) {
+        this.tmpId = tmpId;
+    }
+
+    public String getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
     }
 }
