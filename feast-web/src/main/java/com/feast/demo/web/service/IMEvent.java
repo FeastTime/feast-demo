a7d116bf14ad2e8e9ad6faec2cc340768b7d8ea8
Xiaoqing.Li <xiaoqing.li@ipinyou.com>
2018-02-26 16:37:25 +0800

    添加聊天消息类型

---
+++ feast-web/src/main/java/com/feast/demo/web/service/IMEvent.java
@@ -0,0 +1,32 @@
+package com.feast.demo.web.service;
+
+/**
+ *
+ * Created by LiXiaoQing on 2018/1/29.
+ */
+public class IMEvent {
+
+    public static final String CHAT_TEXT = "CM:chatText";
+
+    // 发红包
+    public static final String SEND_RED_PACKAGE = "CM:sendRedPackage";
+
+    // 收到红包
+    public static final String RECEIVED_RED_PACKAGE = "CM:receivedRedPackage";
+
+    // 打开红包
+    public static final String OPEN_RED_PACKAGE = "CM:openRedPacket";
+
+    // 获得红包奖励
+    public static final String RECEIVED_RED_PACKAGE_SURPRISED = "CM:receivedRedPackageSurprised";
+
+    // 用餐人数变更通知
+    public static final String WAITING_USER_CHANGED = "CM:waitingUserChanged";
+
+    // 用户进店
+    public static final String ENTER_STORE = "CM:enterStore";
+
+
+
+
+}
