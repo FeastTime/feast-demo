fcde7ec05af69d97621a3e862786c45a1f53fee9
Xiaoqing.Li <xiaoqing.li@ipinyou.com>
2018-02-26 15:22:03 +0800

    聊天消息接收服务添加

--- feast-web/src/main/java/com/feast/demo/web/controller/ImController.java
+++ feast-web/src/main/java/com/feast/demo/web/controller/ImController.java
@@ -1,10 +1,27 @@
 package com.feast.demo.web.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.logging.Logger;
 
 @RestController
-@RequestMapping(value = "/coupon")
+@RequestMapping(value = "/im")
 public class ImController {
+
+    Logger logger = Logger.getLogger(this.getClass().getName());
+
+
+    @RequestMapping(value = "/message",method = RequestMethod.POST,produces="text/html;charset=UTF-8")
+    public String loginUser(@RequestBody String text) {
+
+        String message = text;
+
+        logger.info(message);
+
+        return "";
+    }
+
     
 }
