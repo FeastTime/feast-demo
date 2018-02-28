4d535cec2adbbae0a1082b0780d0dd26f4c068f2
yumiao <m15101018347@163.com>
2018-02-28 11:25:13 +0800

    no message

--- feast-web/src/main/java/com/feast/demo/web/controller/RedPackageController.java
+++ feast-web/src/main/java/com/feast/demo/web/controller/RedPackageController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/redPackage")
 public class RedPackageController {
 
+    
 }
